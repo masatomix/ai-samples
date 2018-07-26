d7cbe3623fa4dbd62e7cb87af570a9e909e8debd
Masatomi KINO <masatomix@ki-no.org>
2018-07-26 22:49:49 +0900

    Mecabを使用するための環境設定を追記

--- script/provision.sh
+++ script/provision.sh
@@ -10,9 +10,9 @@ sudo apt install -y python-pip
 sudo apt install -y python3-pip
 
 # install mecab
-#sudo apt install -y mecab libmecab-dev mecab-ipadic-utf8
+sudo apt install -y mecab libmecab-dev mecab-ipadic-utf8
 
 # install mecab-ipadic-neologd  -n(更新する) -y(聞かない)
-#git clone https://github.com/neologd/mecab-ipadic-neologd.git
-#cd mecab-ipadic-neologd/
-#sudo ./bin/install-mecab-ipadic-neologd -n -y
+git clone https://github.com/neologd/mecab-ipadic-neologd.git
+cd mecab-ipadic-neologd/
+sudo ./bin/install-mecab-ipadic-neologd -n -y
