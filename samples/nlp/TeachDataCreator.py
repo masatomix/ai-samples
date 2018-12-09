9a8af857d986fd60537833e46cadc780e9ae60de
Masatomi KINO <masatomix@ki-no.org>
2018-12-09 16:16:11 +0900

    samples/auto_create_teach.py は samples/IREXTeachDataCreator.py とほぼ重複するので削除
    あとはコメントの整理と、
    samples/MecabFacade.py Mecabの戻りの表が、列が足りないときに、''でpaddingするしないのオプションを追加。
    samples/TextReader.py は、最初は文字列をもらっていたのが、ファイルもURLからもコーパスデータを取得できるよう改修

--- samples/nlp/TeachDataCreator.py
+++ samples/nlp/TeachDataCreator.py
@@ -2,60 +2,52 @@
 # -*- coding: utf-8 -*-
 
 
-import requests
-import json
-from MecabFacade import MecabFacade
+from nlp.MecabFacade import MecabFacade
 import configparser
 import hashlib
 import os
-
-from logging import getLogger, StreamHandler, Formatter, DEBUG, INFO
+from logging import getLogger
 
 logger = getLogger(__name__)
-handler = StreamHandler()
-handler_format = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(handler_format)
-
-handler.setLevel(DEBUG)
-logger.setLevel(DEBUG)
-logger.addHandler(handler)
-logger.propagate = False
 
 
-class IREXTeachDataCreator(object):
-    def __init__(self, options='-Ochasen -d /usr/lib/mecab/dic/mecab-ipadic-neologd'):
-        self.__mecab_facade = MecabFacade(options)
+class TeachDataCreator(object):
+    """
+    文章を受け取り、その文章をMecabをつかって形態素解析したり(create_morphs)、
+    文章を受け取り、その文章をGooラボをつかって固有表現抽出したり(create_entities)する
+
+    また、文章を受け取り、Gooラボに固有表現抽出させ、同じ文章をMecabで形態素解析して、Gooラボの結果を
+    IOBタグとして付与する関数(create_teach_data) を提供する。
+
+    またまた、渡された結果データを、指定したディレクトリにテキスト出力する機能(save_teach_data)もつけた。
+    """
+
+    def __init__(self, options='-Ochasen -d /usr/lib/mecab/dic/mecab-ipadic-neologd', padding=True):
+        self.__mecab_facade = MecabFacade(options, padding)
         self.__config = configparser.ConfigParser()
         self.__config.read("./config/config.ini")
 
     def create_morphs(self, sentence):
+        """
+        Mecab をつかって、文章を形態素解析して、返す。
+        :param sentence: 文章
+        :return: 形態素解析結果の配列
+        """
         return self.__mecab_facade.parse(sentence).results()
 
-    def create_entities(self, sentence):
-        payload_obj = {
-            "app_id": self.__config["payload"]["app_id"],
-            "sentence": sentence
-        }
-
-        payload = json.dumps(payload_obj, ensure_ascii=False)
-
-        headers = {
-            "Accept": "application/json",
-            "Content-type": "application/json"
-        }
-
-        proxies, verify = self.get_proxy()
-        r = requests.post(
-            'https://labs.goo.ne.jp/api/entity',
-            data=payload.encode('utf-8').decode('latin-1'),
-            headers=headers,
-            proxies=proxies,
-            verify=verify)
-        return r.json()["ne_list"]
-
-    def create_teach_data(self, sentence):
+    def create_teach_data(self, sentence, entities=None):
+        """
+        文章を形態素解析した結果と、Gooラボが返してきた固有表現抽出結果をもとに、教師データを作成する
+        :param sentence:
+        :param entities:
+        :return:
+        """
         morphs = self.create_morphs(sentence)
-        entities = self.create_entities(sentence)
+        if entities is None:
+            entities = []
+        # 本来entitiesのデフォルト値Noneが不要かな、、、。
+
+        logger.debug(entities)
 
         buf = ""
         morph_index = 0
@@ -134,14 +126,36 @@ class IREXTeachDataCreator(object):
 
         return morphs
 
-    def save_teach_data(self, morphs, dir):
+    def save_teach_data(self, morphs, dir_name, file_name=None):
 
-        os.makedirs(dir, exist_ok=True)
-        path = os.path.join(dir, hashlib.md5(repr(morphs).encode('utf-8')).hexdigest() + ".txt")
+        os.makedirs(dir_name, exist_ok=True)
+
+        if file_name is None:
+            path = os.path.join(dir_name, hashlib.md5(repr(morphs).encode('utf-8')).hexdigest() + ".txt")
+        else:
+            path = os.path.join(dir_name, file_name)
+
         morphsStr = ['\t'.join(s) for s in morphs]
 
         with open(path, mode='w') as f:
             f.write('\n'.join(morphsStr))
+
+    def concat_teach_data(self, dir):
+        import glob
+        path_pattern = os.path.join(dir, "*.txt")
+        paths = glob.glob(path_pattern)
+
+        datas = []
+        for path in paths:
+            with open(path) as f:
+                s = f.read()
+                datas.append(s)
+                datas.append('\n')
+                datas.append('\n')
+
+        save_path = os.path.join(dir, "result.txt")
+        with open(save_path, mode='w') as f:
+            f.write(''.join(datas))
 
     def get_proxy(self):
         verify = True
